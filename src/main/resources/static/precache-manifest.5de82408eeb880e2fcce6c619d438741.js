1c76accbb00c89dcd295082017513964acc4dc98
Jin <cjy1521@naver.com>
2020-05-20 01:54:32 +0900

    리액트+스프링부트+마리아DB

---
+++ src/main/resources/static/precache-manifest.5de82408eeb880e2fcce6c619d438741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f22681f8873780d69b67465c525581f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f958f486f1e09ff7ca3c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "deac2398da8192366447",
+    "url": "/static/js/2.9971d9db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9971d9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f958f486f1e09ff7ca3c",
+    "url": "/static/js/main.f30c0265.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
